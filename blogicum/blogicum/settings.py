9f83f2233180adf03f1cc4b54321b8e3a240b229
gohy279 <gohy20062@mail.ru>
2025-12-13 14:04:42 +0400

    chore: remove comment

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -131,7 +131,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Отправка писем
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
