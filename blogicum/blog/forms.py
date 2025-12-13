5fb7c70c11c943938327f6686998f3d1e270a11f
gohy279 <gohy20062@mail.ru>
2025-12-14 01:55:34 +0400

    fix: add is_published option

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -5,7 +5,8 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ("title", "text", "pub_date", "location", "category", "image")
+        fields = ("title", "text", "pub_date", "location",
+                  "category", "image", "is_published")
         widgets = {
             "pub_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
         }
